d9f9ea28fbeffe3103bccd08cc9d76d21492afed
Jason Gilmore <wj@wjgilmore.com>
2020-04-06 12:06:29 -0400

    First commit

---
+++ app/Commands/ListServices.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Http;
+use LaravelZero\Framework\Commands\Command;
+
+class ListServices extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'services:list {environment}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Lists services';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $environment = $this->argument('environment');
+
+        $environmentBaseUri = getenv(strtoupper($environment));
+        $environmentKey = getenv(strtoupper($environment)."_KEY");
+
+        $response = Http::withHeaders(
+            [
+                'X-DreamFactory-Api-Key' => $environmentKey
+            ]
+        )->get($environmentBaseUri . "system/service?fields=id,name");
+
+        $responseArray = json_decode($response->getBody()->getContents(), true);
+
+        $headers = ["Name", "ID"];
+
+        $this->table($headers, $responseArray['resource']);
+
+        // foreach ($responseArray->resource as $ra) {
+
+        //     echo $ra->name . ": " . $ra->id . PHP_EOL;
+        // }
+
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
